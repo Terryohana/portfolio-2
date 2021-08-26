04b429a12e9a76589bc6020a71b28252229022e9
Matt Yates <matt.yates52@gmail.com>
2021-08-26 12:09:27 +0100

    added placeholder cyclist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { TextSlideLineComponent } from './components/atoms/text-slide-line/text-
 import { DimComponent } from './components/atoms/dim/dim.component';
 import { HeadingComponent } from './components/atoms/heading/heading.component';
 import { ContactComponent } from './components/molecules/contact/contact.component';
+import { CyclistComponent } from './components/atoms/cyclist/cyclist.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { ContactComponent } from './components/molecules/contact/contact.compone
     DimComponent,
     HeadingComponent,
     ContactComponent,
+    CyclistComponent,
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/components/atoms/cyclist/cyclist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cyclist',
+  templateUrl: './cyclist.component.html',
+  styleUrls: ['./cyclist.component.scss']
+})
+export class CyclistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
